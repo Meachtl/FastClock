4a8e1e77fa31b0b5af69039a7e35828ea82cdf2b
Martin Pechak <martin.pechak@gmail.com>
2018-12-18 22:10:43 +0100

    code ready to use

--- EventsDemo.FastClock/FastClock.cs
+++ EventsDemo.FastClock/FastClock.cs
@@ -5,16 +5,80 @@ namespace EventsDemo.FastClock
 {
     public class FastClock
     {
+        #region Fields
+        private DispatcherTimer _dispatcherTimer;
+        private static FastClock _instance;
+        #endregion
+
         #region Properties
-        public int Factor { get; set; }
+        public int Factor
+        {
+            set
+            {
+                if (0 < value && value <= 60000)
+                {
+                    _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(60000 / value);
+                }                
+            }
+        }
 
-        public int Instance { get; set; }
+        static public FastClock Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new FastClock();
+                }
+                return _instance;
+            }
+        }
 
-        public bool IsRunning { get; set; }
+        public bool IsRunning
+        {
+            set
+            {
+                _dispatcherTimer.IsEnabled = value;
+            }
+            get
+            {
+                return _dispatcherTimer.IsEnabled;
+            }
+        }
 
         public DateTime Time { get; set; }
         #endregion
 
+        #region Constructor
+        private FastClock()
+        {
+            _dispatcherTimer = new DispatcherTimer();
+
+            _dispatcherTimer.Tick += Timer_Tick;
+            //_instance.OneMinuteIsOver += OnOneMinuteIsOver;
+        }
+
+
+        #endregion
+
+        //private void OnOneMinuteIsOver(object sender, DateTime time)
+        //{
+        //    //while (IsRunning)
+        //    //{
+        //    //    OneMinuteIsOver?.Invoke(this, time);
+        //    //}
+        //}
+
+        private void Timer_Tick(object sender, EventArgs args)
+        {
+            Time = Time.AddMinutes(1);
+            OneMinuteIsOver?.Invoke(this, Time);
+
+            //_dispatcherTimer.Interval = new TimeSpan(0, 0, Factor);
+            //_dispatcherTimer.Start();
+        }
+
+        public event EventHandler<DateTime> OneMinuteIsOver;
 
     }
 }
--- EventsDemo.FastClockWpf/MainWindow.xaml.cs
+++ EventsDemo.FastClockWpf/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ namespace EventsDemo.FastClockWpf
         public MainWindow()
         {
             InitializeComponent();
+            FastClock.FastClock.Instance.OneMinuteIsOver += FastClockOneMinuteIsOver;
+
         }
 
         private void MetroWindow_Initialized(object sender, EventArgs e)
@@ -21,28 +23,51 @@ namespace EventsDemo.FastClockWpf
 
         private void ButtonSetTime_Click(object sender, RoutedEventArgs e)
         {
-            DateTime date = (DateTime)(DatePickerDate.SelectedDate);
-            DateTime time = DateTime.Parse(TextBlockTime.Text);
+            
 
-
-
-            DatePickerDate.SelectedDate = new DateTime(2015, 07, 17);
-            TextBoxTime.Text = (new DateTime(2015, 07, 17, 12, 34, 00).ToShortTimeString());
+            //DatePickerDate.SelectedDate = new DateTime(2015, 07, 17);
+            //TextBoxTime.Text = (new DateTime(2015, 07, 17, 12, 34, 00).ToShortTimeString());
 
             SetFastClockStartDateAndTime();
         }
 
         private void SetFastClockStartDateAndTime()
         {
+            //DateTime date = (DateTime)(DatePickerDate.SelectedDate);
+            //DateTime time = DateTime.Parse(TextBlockTime.Text);
+
+            TextBlockDate.Text = DatePickerDate.Text;
+            TextBlockTime.Text = TextBoxTime.Text;
+            FastClock.FastClock.Instance.Time = DateTime.Parse(DatePickerDate.Text + " " + TextBoxTime.Text);
 
         }
 
         private void FastClockOneMinuteIsOver(object sender, DateTime fastClockTime)
         {
+            TextBlockDate.Text = FastClock.FastClock.Instance.Time.ToShortDateString();
+            TextBlockTime.Text = FastClock.FastClock.Instance.Time.ToShortTimeString();
         }
 
         private void CheckBoxClockRuns_Click(object sender, RoutedEventArgs e)
         {
+            SliderFactor.IsEnabled = !CheckBoxClockRuns.IsChecked.Value;
+            TextBoxFactor.IsEnabled = !CheckBoxClockRuns.IsChecked.Value;
+            ButtonSetTime.IsEnabled = !CheckBoxClockRuns.IsChecked.Value;
+
+
+            if (CheckBoxClockRuns.IsChecked.Value)
+            {
+                //SliderFactor.IsEnabled = false;
+
+                int.TryParse(TextBoxFactor.Text, out int factor);
+                FastClock.FastClock.Instance.Factor = factor;
+
+                FastClock.FastClock.Instance.IsRunning = true;
+            }
+            else
+            {
+                FastClock.FastClock.Instance.IsRunning = false;                
+            }
         }
 
         private void ButtonCreateView_Click(object sender, RoutedEventArgs e)
@@ -51,5 +76,7 @@ namespace EventsDemo.FastClockWpf
             digitalClock.Owner = this;
             digitalClock.Show();
         }
+
+       
     }
 }
